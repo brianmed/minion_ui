f0af50cce6eaa24fb99d4708cadd4cb72fd84eed
Brian Medley <bpm@bmedley.org>
2015-10-04 07:16:55 -0400

    Enqueue and Retry a job

--- minion_ui.pl
+++ minion_ui.pl
@@ -2,7 +2,7 @@
 
 use Mojolicious::Lite;
 
-use Mojo::JSON qw(encode_json);
+use Mojo::JSON qw(encode_json decode_json);
 
 plugin Minion => {Pg => "postgresql://$ENV{DBI_USER}:$ENV{DBI_PASS}\@127.0.0.1/jobs"};
 
@@ -74,6 +74,26 @@ get '/v1/jobs/stats' => sub {
     $c->render(json => $ret);
 };
 
+get '/v1/job/enqueue' => sub {
+    my $c = shift;
+
+    my $task = $c->param("task");
+    my $queue = $c->param("queue");
+    my $priority = $c->param("priority");
+    my $delay = $c->param("delay");
+    my $args = decode_json($c->param("args") || "[]");
+
+    my $options = {};
+
+    $options->{priority} = $priority if $priority;
+    $options->{delay} = $delay if $delay;
+    $options->{queue} = $queue if $queue;
+
+    my $job_id = $c->app->minion->enqueue($task, $args, $options);
+
+    $c->render(json => {job_id => $job_id});
+};
+
 app->start;
 
 __DATA__
@@ -84,6 +104,8 @@ __DATA__
 <html>
 <head>
     <title></title>
+    <link rel="stylesheet" href="http://kendo.cdn.telerik.com/2015.3.930/styles/kendo.common.core.min.css" />
+    <link rel="stylesheet" href="http://kendo.cdn.telerik.com/2015.3.930/styles/kendo.common-nova.core.min.css" />
     <link rel="stylesheet" href="http://kendo.cdn.telerik.com/2015.3.930/styles/kendo.mobile.all.min.css" />
     <link rel="stylesheet" href="/epoch.min.css" />
     <link rel="stylesheet" href="/style.css" />
@@ -145,15 +167,15 @@ __DATA__
     </div>
 </div>
 
-<div data-role="view" id="tabstrip-minion" data-title="Minion" data-layout="mobile-tabstrip" data-model="model.minion" data-init="model.common.init">
+<div data-role="view" id="tabstrip-minion" data-title="Minion" data-layout="mobile-tabstrip" data-model="model.minion" data-init="model.minion.init">
     <ul id="listview-minion">
         <script type="text/x-kendo-template" id="listview-minion-template">
             #: text #: #: value #
         </script>
     </ul>
-</div>
 
-<div data-role="view" id="tabstrip-workers" data-title="Workers" data-layout="mobile-tabstrip" data-model="model.workers" data-init="model.common.init">
+    <center><p><h1>Workers</h1></p></center>
+
     <ul id="listview-workers">
         <script type="text/x-kendo-template" id="listview-workers-template">
             # if (data.text) { #
@@ -165,18 +187,73 @@ __DATA__
     </ul>
 </div>
 
-<div data-role="view" id="tabstrip-jobs" data-title="Jobs" data-layout="mobile-tabstrip" data-model="model.jobs" data-init="model.common.init">
+<div data-role="view" id="tabstrip-jobs" data-title="Jobs" data-layout="mobile-tabstrip" data-model="model.jobs" data-init="model.jobs.init">
     <ul id="listview-jobs">
         <script type="text/x-kendo-template" id="listview-jobs-template">
+            <a data-role="button" class="km-primary km-button-right km-icon-button"><span class="km-icon km-refresh"></span></a>
             # if (data.text) { #
                 #: data.text #
             # } else { #
-                #: id #: #: task # [#: queue #]<br>
+                #: task #: [#: queue #]: [#: id #]<br>
                 State: #: state #<br>
                 Args: <code>#: args #</code>
             # } #
         </script>
     </ul>
+</div>
+
+<div data-role="view" id="tabstrip-actions" data-title="Actions" data-layout="mobile-tabstrip" data-use-native-scrolling="true" data-model="model.actions" data-show="model.actions.show">
+     <form>
+        <ul data-role="listview" data-style="inset">
+            <li>
+                <label class="km-required km-label-above"><span>Task name</span>
+                    <input value="" type="text" name="task" required>
+                </label>
+            </li>
+            <li>
+                <label class="km-label-above">Queue Name
+                    <input type="text" value="" name="queue">
+                </label>
+            </li>
+            <li>
+                <label class="km-label-above">Priority
+                    <input value="" type="text" name="priority">
+                </label>
+            </li>
+            <li>
+                <label class="km-label-above">Delay
+                    <input value="" type="text" name="delay">
+                </label>
+            </li>
+            <li>
+                <fieldset>
+                    <legend>Arguments</legend>
+                    <textarea style="height: 50px;" placeholder="JSON goes here" name="args" class="k-valid"></textarea>
+                </fieldset>
+            </li>          
+        </ul>
+    </form>
+
+    <center><a data-role="button" data-bind="events:{ click: submit }" data-animated="true" data-icon="action" class="km-primary" style="margin-top: 20px;">Submit</a></center>
+</div>
+
+<div data-role="modalview" id="modalview-job-enqueue" style="width: 60%;" data-model="model.actions">
+    <div data-role="header">
+        <div data-role="navbar">
+            <span>Results</span>
+            <a data-bind="events:{ click: close }" data-role="button" data-align="right">Close</a>
+        </div>
+    </div>
+    
+    <script type="text/x-kendo-template" id="enqueue-template">
+        <center>
+        <h1>#: task #</h1>
+
+        Enqueued into #: queue # as job id #: job_id #
+        </center>
+    </script>
+
+    <div id="enqueue-results"></div>
 </div>
 
 <div data-role="layout" data-id="mobile-tabstrip">
@@ -192,29 +269,37 @@ __DATA__
         <div data-role="tabstrip">
             <a href="#tabstrip-dashboard" data-icon="globe">Dashboard</a>
             <a href="#tabstrip-minion" data-icon="share">Minion</a>
-            <a href="#tabstrip-workers" data-icon="settings">Workers</a>
             <a href="#tabstrip-jobs" data-icon="history">Jobs</a>
+            <a href="#tabstrip-actions" data-icon="action">Actions</a>
         </div>
     </div>
 </div>
 
 <script>
     var model = kendo.observable({
-        common: {
+        minion: {
             init: function(e) {
-                var model = e.view.model;
-                var id = model.listview.id;
-                var template = model.listview.template;
+                var m = e.view.model;
+                var id = m.listview.id;
+                var template = m.listview.template;
 
                 $(id).kendoMobileListView({
-                    dataSource: model.dataSource,
+                    dataSource: m.dataSource,
+                    pullToRefresh: true,            
+                    template: $(template).text(),
+                });
+
+                m = model.workers;
+                var id = m.listview.id;
+                var template = m.listview.template;
+
+                $(id).kendoMobileListView({
+                    dataSource: m.dataSource,
                     pullToRefresh: true,            
                     template: $(template).text(),
                 });
             },
-        },
 
-        minion: {
             listview: {
                 id: "#listview-minion",
                 template: "#listview-minion-template",
@@ -275,6 +360,27 @@ __DATA__
         },
 
         jobs: {
+            init: function(e) {
+                var model = e.view.model;
+                var id = model.listview.id;
+                var template = model.listview.template;
+
+                $(id).kendoMobileListView({
+                    dataSource: model.dataSource,
+                    pullToRefresh: true,            
+                    template: $(template).text(),
+                    click: function (e) {
+                        var params = $.param({
+                            task: e.dataItem.task,
+                            priority: e.dataItem.priority,
+                            queue: e.dataItem.queue,
+                            args: e.dataItem.args,
+                        });
+                        app.navigate("#tabstrip-actions?" + params, "slide");
+                    },
+                });
+            },
+
             listview: {
                 id: "#listview-jobs",
                 template: "#listview-jobs-template",
@@ -287,6 +393,60 @@ __DATA__
                     }
                 }
             })
+        },
+
+        actions: {
+            show: function(e) {
+                console.log(e.view.params);
+
+                if (e.view.params.task) {
+                    var element = app.view().element;
+
+                    var form = $(element).find("form");
+
+                    $(element).find("input[name='task']").val(e.view.params.task);
+                    $(element).find("input[name='queue']").val(e.view.params.queue);
+                    $(element).find("input[name='priority']").val(e.view.params.priority);
+                    $(element).find("textarea[name='args']").val(e.view.params.args);
+                }
+            },
+            submit: function(e) {
+                var element = app.view().element;
+
+                var form = $(element).find("form");
+
+                var validator = $(form).kendoValidator({
+                    errorTemplate: '',
+                }).data("kendoValidator");
+
+                $(element).find("form").find("span").css("color", "");
+                if (!validator.validate()) {
+                    $(element).find("form").find("span").css("color", "red");
+                    return;
+                }
+
+                app.showLoading();
+
+                var formData = $(form).serialize();
+                console.log(formData);
+
+                $.getJSON("<%= url_for("/v1/job/enqueue") %>?" + formData, function(result) {
+                    var template = kendo.template($('#enqueue-template').text());
+                    var html = template({
+                        task: $(element).find("input[name='task']").val(),
+                        queue: $(element).find("input[name='queue']").val() || "default",
+                        job_id: result.job_id
+                    });
+                    $('#enqueue-results').html(html);
+
+                    app.hideLoading();
+
+                    $('#modalview-job-enqueue').data("kendoMobileModalView").open();
+                });
+            },
+            close: function(e) {
+                $('#modalview-job-enqueue').data("kendoMobileModalView").close();
+            },
         },
 
         dashboard: {
@@ -394,7 +554,7 @@ __DATA__
     });
 
     var app = new kendo.mobile.Application(document.body, { 
-        skin: "flat",
+        skin: "nova",
 
         init: function () {
             kendo.bind($("#tabstrip-minion"), model.minion);
