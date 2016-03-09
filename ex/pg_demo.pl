aebd0470605736816adda7f84fcc88d901523a1d
Brian Medley <bpm@bmedley.org>
2016-03-08 19:04:57 -0500

    Pg example with AccessLog

--- ex/pg_demo.pl
+++ ex/pg_demo.pl
@@ -2,6 +2,15 @@ use Mojolicious::Lite;
 
 plugin Minion => {Pg => "postgresql://$ENV{DBI_USER}:$ENV{DBI_PASS}\@127.0.0.1/jobs"};
 
+eval {
+    if (-d app->home->rel_dir("log")) {
+        plugin(AccessLog => {log => app->home->rel_file("log/access.log"), format => '%h - %u %t "%r" %>s %b %D "%{Referer}i" "%{User-Agent}i"'});
+    }
+    else {
+        plugin(AccessLog => {log => \*STDERR, format => '%h - %u %t "%r" %>s %b %D "%{Referer}i" "%{User-Agent}i"'});
+    }
+};
+
 app->minion->add_task(slow_log => sub {
     my ($job, $msg) = @_;
 
